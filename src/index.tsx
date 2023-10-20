0c85444b0b5967578097ef33375e7853fda55c61
analogdev.eth <dejijolaoluwa@gmail.com>
2023-10-20 01:40:02 +0100

    add fingerprint pad to homepage, define global styles in styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
 // pages
 import Home from "./pages/Home";
@@ -9,8 +10,32 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import ErrorPage from "./pages/Error";
 
-// stylesheet
-import "./index.css";
+// fonts
+import tyewriter from "./fonts/typewriter.ttf";
+import courier from "./fonts/courier.ttf";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'typewriter';
+    src: url(${tyewriter}) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'courier';
+    src: url(${courier}) format('truetype');
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'courier';
+  }
+
+  .old-font, .old-font * {
+    font-family: 'typewriter';
+  }
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -38,6 +63,7 @@ const router = createBrowserRouter([
 
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,93 @@
 import React from "react";
+import styled from "styled-components";
 
 // components
 import Nav from "../components/Nav";
 import Socials from "../components/Socials";
 
+// assets
+import dummyPrint from "../assets/dummy-print.png";
+import barcode from "../assets/barcode.png";
+
 // types
 import { Pages } from "../types";
 
+const Outline = styled.div``;
+
+const FingerPrintPad = styled.div`
+  margin: auto;
+  position: relative;
+  // border: 1px solid red;
+  width: 30rem;
+  text-align: center;
+
+  h5 {
+    margin-bottom: .5rem;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+    font-size: 2.5rem;
+  }
+
+  ul {
+    margin-left: 2.8rem;
+  }
+
+  li {
+    text-align: left;
+    list-style-type: none;
+    margin-bottom: 0.7rem;
+    font-size: 1rem;
+  }
+
+  > div {
+    text-align: right;
+  }
+
+  > div p {
+    margin-right: 1rem;
+  }
+`;
+
+const DummyPrint = styled.img`
+  position: absolute;
+  top: .5rem;
+  right: 1rem;
+  width: 8rem;
+  z-index: -1;
+`;
+
+const Barcode = styled.img`
+  width: 50%;
+  margin-right: 1rem;
+`;
+
 const Home = (): JSX.Element => {
   return (
-    <div>
+    <Outline>
       <Nav page={Pages.Home} />
 
-      <ul>
-        <li>Codeographer Introduction</li>
-      </ul>
+      <FingerPrintPad className="old-font">
+        <DummyPrint src={dummyPrint} alt="fingerprint" />
+        <h5>
+          thecodeographer<span>&#174;</span>
+        </h5>
+        <h1>FingerPrint Pad</h1>
+        <ul>
+          <li>- lorem ipsum dolor sit amet.</li>
+          <li>- clear, crisp imprint.</li>
+          <li>- use on any paper.</li>
+        </ul>
+        <div>
+          <p>Item #717061</p>
+          <Barcode src={barcode} alt="" />
+          <pre>73775 03027 269      3</pre>
+        </div>
+      </FingerPrintPad>
 
       <Socials />
-    </div>
+    </Outline>
   );
 };
 
